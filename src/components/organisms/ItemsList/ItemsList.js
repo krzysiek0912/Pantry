b8ccfe6c827b916477f93d7f5a52c2a79773f23b
Krzysztof Branecki <krzysiek091290@gmail.com>
2020-04-05 18:25:14 +0200

    Add info about empty lists

--- src/components/organisms/ItemsList/ItemsList.js
+++ src/components/organisms/ItemsList/ItemsList.js
@@ -1,39 +1,44 @@
 import React from 'react';
 import AppContext from '../../../context';
 import SingleItem from '../../molecules/SingleItem/SingleItem';
+import MessageBox from '../../atoms/MessageBox/MessageBox';
 const ItemsList = ({ items }) => {
     return (
-        <table className="min-w-full leading-normal">
-            <thead>
-                <tr>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                        Produkt
-                    </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                        Ilość
-                    </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                        Minimalna Ilość
-                    </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                        Akcja
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <AppContext.Consumer>
-                    {(context) => {
-                        return context.products.map((product) => (
-                            <SingleItem
-                                key={product.id}
-                                product={product}
-                                toggleModal={context.toggleModal}
-                            ></SingleItem>
-                        ));
-                    }}
-                </AppContext.Consumer>
-            </tbody>
-        </table>
+        <AppContext.Consumer>
+            {({ products, toggleModal }) => {
+                return products.length > 0 ? (
+                    <table className="min-w-full leading-normal">
+                        <thead>
+                            <tr>
+                                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    Produkt
+                                </th>
+                                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    Ilość
+                                </th>
+                                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    Minimalna Ilość
+                                </th>
+                                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    Akcja
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {products.map((product) => (
+                                <SingleItem
+                                    key={product.id}
+                                    product={product}
+                                    toggleModal={toggleModal}
+                                ></SingleItem>
+                            ))}
+                        </tbody>
+                    </table>
+                ) : (
+                    <MessageBox noClose>brak produktów</MessageBox>
+                );
+            }}
+        </AppContext.Consumer>
     );
 };
 
--- src/components/organisms/ShopList/ShopList.js
+++ src/components/organisms/ShopList/ShopList.js
@@ -1,39 +1,45 @@
 import React from 'react';
 import AppContext from '../../../context';
 import SingleItem from '../../molecules/SingleItem/SingleItem';
+import MessageBox from '../../atoms/MessageBox/MessageBox';
+
 const ShopList = () => {
     return (
-        <table className="min-w-full leading-normal">
-            <thead>
-                <tr>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                        Produkt
-                    </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                        Ilość
-                    </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                        Braki
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <AppContext.Consumer>
-                    {(context) => {
-                        return context.products
-                            .filter(({ count, minCount }) => count < minCount)
-                            .map((product) => (
-                                <SingleItem
-                                    key={product.id}
-                                    product={product}
-                                    removeItem={context.removeItem}
-                                    isShopList
-                                ></SingleItem>
-                            ));
-                    }}
-                </AppContext.Consumer>
-            </tbody>
-        </table>
+        <AppContext.Consumer>
+            {({ products, removeItem }) => {
+                return products.length > 0 ? (
+                    <table className="min-w-full leading-normal">
+                        <thead>
+                            <tr>
+                                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    Produkt
+                                </th>
+                                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    Ilość
+                                </th>
+                                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    Braki
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {products
+                                .filter(({ count, minCount }) => count < minCount)
+                                .map((product) => (
+                                    <SingleItem
+                                        key={product.id}
+                                        product={product}
+                                        removeItem={removeItem}
+                                        isShopList
+                                    ></SingleItem>
+                                ))}
+                        </tbody>
+                    </table>
+                ) : (
+                    <MessageBox noClose>brak produktów</MessageBox>
+                );
+            }}
+        </AppContext.Consumer>
     );
 };
 
